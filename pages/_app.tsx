c610aae3f0050df6b2e7086001d970556c4df750
Ken Duigraha Putra <kenduigraha@gmail.com>
2024-11-12 21:22:26 +0700

    Change logic main layout.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { usePathname } from 'next/navigation';
 import "./styles/index.css";
 import MainLayout from "./components/main/MainLayout";
+import MainPage from "./components/main/MainPage";
 
 export default function MyApp({ Component, pageProps }) {
+    const navigationRoute = usePathname();
     return (
         <MainLayout>
+            {navigationRoute === '/' && (<MainPage />)}
             <Component {...pageProps} />
         </MainLayout>
     )
